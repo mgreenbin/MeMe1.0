3a69b59ca710182631f4bc2e6d748bdb46ed788a
Mark Greenberg <magreenberg@gmail.com>
2019-09-17 16:28:19 -0500

    Initial Commit

---
+++ MeMe1.0/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MeMe1.0
+//
+//  Created by Mark Greenberg on 9/17/19.
+//  Copyright © 2019 MarkStar Technologies. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
